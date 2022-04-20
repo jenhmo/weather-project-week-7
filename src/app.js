300d4c14f93eb9ff8ef8d5bda13ed15816278712
jenhmo <101249416+jenhmo@users.noreply.github.com>
2022-04-20 21:29:19 +0200

    added css

--- src/app.js
+++ src/app.js
@@ -39,8 +39,6 @@ function displayTemp(response){
     let iconElement = document.querySelector(`#icon`);
     iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
-
-    getForecast (response.data.coord);
 };
 
 function search(city){
